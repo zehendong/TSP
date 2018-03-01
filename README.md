b08db04a4b33605255e3cd2d1d7c4fea9245d896
zehendong <zehengdong@gmail.com>
2018-03-01 20:39:34 +0800

    [add] add airline-prediction in LSTM

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TSP
+
+This is Time series prediction
---
+++ module/constants.py
@@ -0,0 +1,9 @@
+# CONSTANTS NEED TO MODIFY
+
+# data directory that contains the training / test data
+DATA_HOME = "F:\Projects\Git\TSP\data"
+ARILINE_DATA = DATA_HOME + "\international-airline-passengers.csv"
+VEHICLE_DATA = DATA_HOME + "\\vehicle_data.csv"
+
+# config
+BATCH_SIZE = 512
